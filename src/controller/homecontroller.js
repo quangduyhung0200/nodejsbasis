7a1cb772c0b418c99cbc9897728830c999a453e9
quangduyhung0200 <hung321chiengden@email.com>
2023-06-14 19:32:39 +0700

    tao nguoi dung

--- src/controller/homecontroller.js
+++ src/controller/homecontroller.js
@@ -12,8 +12,16 @@ let getDetailpage = async (req, res) => {
     let userid = req.params.userId;
     console.log(userid);
     let user = await pool.execute(`SELECT * FROM users WHERE id = ?`, [userid]);
+
     return res.send(JSON.stringify([user[0]]));
 }
+let createNewUser = async (req, res) => {
+
+    let { fistname, lastname, email, address } = req.body;
+
+    await pool.execute(`insert into users(fistName,lastName,email,address) values (?,?,?,?) `, [fistname, lastname, email, address]);
+    return res.redirect('/');
+}
 module.exports = {
-    getHomepage, getDetailpage
+    getHomepage, getDetailpage, createNewUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -1,8 +1,10 @@
 import express from "express";
 import homeController from '../controller/homecontroller';
+import { Route } from "express";
 let router = express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
+    router.post('/create', homeController.createNewUser)
     router.get('/detail/user/:userId', homeController.getDetailpage)
     router.get('/about', (req, res) => {
         res.send(`i'm hung`);
