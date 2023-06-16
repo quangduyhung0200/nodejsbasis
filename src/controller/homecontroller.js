0c48bed0564764a16adf1f1bb141d14ed758a085
quangduyhung0200 <hung321chiengden@email.com>
2023-06-16 19:39:56 +0700

    update

--- src/controller/homecontroller.js
+++ src/controller/homecontroller.js
@@ -18,10 +18,28 @@ let getDetailpage = async (req, res) => {
 let createNewUser = async (req, res) => {
 
     let { fistname, lastname, email, address } = req.body;
+    console.log(fistname);
 
     await pool.execute(`insert into users(fistName,lastName,email,address) values (?,?,?,?) `, [fistname, lastname, email, address]);
     return res.redirect('/');
 }
+let deleteUser = async (req, res) => {
+    let Id = req.body.userIddelete
+    await pool.execute(`delete from users where id=?`, [Id]);
+    return res.redirect('/');
+}
+let editUser = async (req, res) => {
+    let iduser = req.params.id;
+    let [user] = await pool.execute(`SELECT * FROM users WHERE id = ?`, [iduser]);
+    return res.render('update.ejs', { dataUser: user[0] });
+
+}
+let updateUser = async (req, res) => {
+    let { fistname, lastname, email, address, id } = req.body;
+    console.log(req.body);
+    await pool.execute('update users set fistName=?, lastName=?,email=?,address=? where id=?', [fistname, lastname, email, address, id])
+    return res.redirect('/');
+}
 module.exports = {
-    getHomepage, getDetailpage, createNewUser
+    getHomepage, getDetailpage, createNewUser, deleteUser, editUser, updateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,15 @@
 import express from "express";
 import homeController from '../controller/homecontroller';
-import { Route } from "express";
+import { Router } from "express";
 let router = express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
+    router.post('/update-user', homeController.updateUser)
     router.post('/create', homeController.createNewUser)
     router.get('/detail/user/:userId', homeController.getDetailpage)
+    router.post('/delete-user', homeController.deleteUser)
+    router.get('/edit-user/:id', homeController.editUser)
+
     router.get('/about', (req, res) => {
         res.send(`i'm hung`);
 
